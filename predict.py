6e76a0f77fe03f6a349d875e384ff1bf2e534c4f
bendangnuksung <bendangnuksungimsong@gmail.com>
2017-07-19 23:00:56 +0530

    Testing and accuracy added

--- predict.py
+++ predict.py
@@ -1,7 +1,8 @@
 from sklearn import tree
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+import numpy as np
+import math
 df=pd.read_csv('test.csv')
 train, test = train_test_split(df, test_size = 0.2)
 clf=tree.DecisionTreeRegressor()
@@ -10,16 +11,19 @@ inp= [float(x) for x in input().split()]
 a=clf.predict([inp])
 print("Predicted %=", a)
 test_value= []
-for x in test:
-	test_value.append(clf.predict([x]))
+np_test=np.asarray(test)
+for x in range(len(test)):
+	test_value.append(clf.predict([np_test[x,0:4]]))
+np_value=np.asarray(test_value)
 testPercen=0
+np_1= np.asarray(test)
 for x in range(len(test_value)):
-	if math.ceil(test_value[x])== math.ceil(test[x][4]):
+	if math.ceil(np_value[x])== math.ceil(np_1[x,4]):
 		testPercen +=1
 accuracy= (testPercen*100)/ len(test_value)
-
-
-import pydotplus 
+print ("Accuracy of the model is ", accuracy)
+#to visualize the model, creates pdf
+'''import pydotplus 
 dot_data = tree.export_graphviz(clf, out_file=None) 
 graph = pydotplus.graph_from_dot_data(dot_data) 
-graph.write_pdf("test.pdf")	
+graph.write_pdf("test.pdf")	'''

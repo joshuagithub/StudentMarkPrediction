328c9fdc993d81ed5fde5b74e4bd55a9ba182322
bendangnuksung <bendangnuksungimsong@gmail.com>
2017-09-19 11:13:21 +0530

    made runmodel()

--- run_model.py
+++ run_model.py
@@ -1,6 +1,8 @@
 import pickle
 from sklearn import tree
-load_model= pickle.load(open('regression_model.sav', 'rb'))
-inp= [float(x) for x in input().split()]
-a=load_model.predict([inp])
-print("Predicted %=", a)
+
+def runmodel(inp):
+	load_model= pickle.load(open('regression_model.sav', 'rb'))
+	a=load_model.predict([inp])
+	return a
+	print("Predicted %=", a)
